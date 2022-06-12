f1f2bcfd86f7277f2bfb49cb46b75210e8b6952c
Matthew Aileru <m.a.aileru@gmail.com>
2022-06-12 01:59:48 +0100

    Added necessary changes for adding favourite advice slices to a favourite list

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const dispenseButton = document.getElementById("dispense-button")
 const saveButton = document.getElementById("save-button")
+const testButton = document.getElementById("test-button")
 const quoteArea = document.querySelector(".quote-area")
 let noteId, noteContent = ""
+const favList = document.getElementById("fav-list")
+let favSet = new Set()
+
+function displayFavList() {
+    for (const [key, value] of Object.entries(sessionStorage)) {
+        // console.log(`${key}: ${value}`)
+        // favSet.add(`${key} - ${value}`)
+        const favEntry = document.createElement("p")
+        favEntry.innerHTML = `${`${key}: ${value}`}`
+        favList.appendChild(favEntry)           
+    }
+    // console.log("Yeah it's working")
+}
 
 dispenseButton.addEventListener("click", async () => {
     let baseUrl = 'https://api.adviceslip.com/advice'
@@ -16,21 +30,35 @@ dispenseButton.addEventListener("click", async () => {
     noteId = data.slip.id
     noteContent = data.slip.advice
 
-    console.log(`${noteId} - ${noteContent}`)
-    } 
-)
+    // console.log(`${noteId} - ${noteContent}`)
+})
 
 saveButton.addEventListener("click", async () => {
-    let baseUrl = 'https://api.adviceslip.com/advice'
-    let response = await fetch(baseUrl)
-    let data = await response.json()
+    if (noteId !== undefined) {
+        console.log(`${noteId} - ${noteContent}`)
+        
+        sessionStorage.setItem(noteId, noteContent)
 
-    // console.log(data.slip.id)
-    // console.log(data.slip.advice)
-    
-    sessionStorage.setItem(data.slip.id, data.slip.advice)
-    localStorage.theKey = "The value"
-    } 
-)
+        // the for..in method
+        // for(const prop in sessionStorage) {
+        //     if (prop !== undefined) {
+        //         console.log(`${prop} - ${sessionStorage[prop]}`)
+        //     }
+        // }
 
-// sessionStorage.setItem("type", "gypsy")
+        // the Object.entries() method
+        // for (const [key, value] of Object.entries(sessionStorage)) {
+        //     console.log(`${key}: ${value}`)
+        //     const favEntry = document.createElement("p")
+        //     favEntry.innerHTML = `${key}: ${value}`
+        //     favList.appendChild(favEntry)
+        // }
+
+        
+
+        // console.log(sessionStorage)
+    }
+})
+
+testButton.addEventListener("click", () => { console.log(sessionStorage ? "yes" : "no") })
+// sessionStorage.clear()
